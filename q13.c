e06bd12f005dc79f1803ae716b27b98566d893c9
AnanyaGhildiyal8 <ananyaa.gh@gmail.com>
2026-02-13 19:37:43 +0530

    Add files via upload

---
+++ q13.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void spiral_traverse(int matrix[][100], int r, int c) {
+    int top = 0;
+    int bottom = r - 1;
+    int left = 0;
+    int right = c - 1;
+    
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            printf("%d ", matrix[top][i]);
+        }
+        top++;
+        
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+        
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                printf("%d ", matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+}
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+    
+    int matrix[100][100];
+    
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    spiral_traverse(matrix, r, c);
+    
+    return 0;
+}
